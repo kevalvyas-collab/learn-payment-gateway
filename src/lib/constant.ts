bd6d7d73477893d2d9ea25293363fd3d57b4f6c9
keval vyas <keval.vyas@spellenx.com>
2025-12-16 11:00:54 +0530

    solve merge conflict in constant file

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,11 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 const ENV_CONFIG = {
-<<<<<<< HEAD
-    SERVER_PORT: process.env.PORT || 7000,
-=======
     SERVER_PORT: process.env.PORT,
->>>>>>> e6b4321 (update folder structure)
     X_WEBHOOK_SIGNATURE: process.env.X_WEBHOOK_SIGNATURE
 };
 const PAY_U_CONFIG = {
